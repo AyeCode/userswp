9a95cf01976b9c0ad0069f110b20c3961dfebce5
wpdev10 <squarelinksolutions@gmail.com>
2018-02-08 18:01:21 +0530

    Modified redirect to referer page after login.

--- templates/login.php
+++ templates/login.php
@@ -11,33 +11,27 @@
                 <?php
                 if (is_uwp_login_page()) {
                     if (isset($_GET['redirect_to'])) {
-                        $redirect_to = strip_tags(esc_sql($_GET['redirect_to']));
-                        ?>
-                        <input type="hidden" name="redirect_to" value="<?php echo $redirect_to; ?>"/>
-                        <?php
+                        $redirect_to = esc_url($_GET['redirect_to']);
                     } else if($referer = wp_get_referer()){
-                        ?>
-                        <input type="hidden" name="redirect_to" value="<?php echo $referer; ?>"/>
-                        <?php
+                        $redirect_to = $referer;
                     }
                 } else {
-                    ?>
-                    <input type="hidden" name="redirect_to" value="<?php echo uwp_current_page_url(); ?>" />
-                    <?php
+                    $redirect_to = uwp_current_page_url();
                 }
                 ?>
+                <input type="hidden" name="redirect_to" value="<?php echo $redirect_to; ?>"/>
                 <?php do_action('uwp_social_fields', 'login'); ?>
                 <div class="uwp-remember-me">
                     <label style="display: inline-block;" for="remember_me">
                         <input name="remember_me" id="remember_me" value="forever" type="checkbox">
-                        <?php echo __( 'Remember Me', 'userswp' ); ?>
+                        <?php _e( 'Remember Me', 'userswp' ); ?>
                     </label>
                 </div>
-                <input name="uwp_login_submit" value="<?php echo __( 'Login', 'userswp' ); ?>" type="submit">
+                <input type="submit" name="uwp_login_submit" value="<?php _e( 'Login', 'userswp' ); ?>">
             </form>
-            <div class="uwp-forgotpsw"><a href="<?php echo uwp_get_page_link('forgot'); ?>"><?php echo __( 'Forgot password?', 'userswp' ); ?></a></div>
+            <div class="uwp-forgotpsw"><a href="<?php echo uwp_get_page_link('forgot'); ?>"><?php _e( 'Forgot password?', 'userswp' ); ?></a></div>
             <div class="clfx"></div>
-            <div class="uwp-register-now"><?php echo __( 'Not a Member?', 'userswp' ); ?> <a rel="nofollow" href="<?php echo uwp_get_page_link('register'); ?>"><?php echo __( 'Create Account', 'userswp' ); ?></a></div>
+            <div class="uwp-register-now"><?php _e( 'Not a member?', 'userswp' ); ?> <a rel="nofollow" href="<?php echo uwp_get_page_link('register'); ?>"><?php _e( 'Create account', 'userswp' ); ?></a></div>
         </div>
     </div>
 <?php do_action('uwp_template_after', 'login'); ?>
