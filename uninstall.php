178def453073ca550c54436f4a310171930e47fa
wpdev10 <squarelinksolutions@gmail.com>
2018-06-07 18:54:49 +0530

    modified uninstall.php for multisite setup

--- uninstall.php
+++ uninstall.php
@@ -20,98 +20,72 @@ if ( !class_exists( 'UsersWP' ) ) {
     include_once( 'userswp.php' );
 }
 
-if ( uwp_get_option('uninstall_erase_data') == '1' ) {
-    $wpdb->hide_errors();
+$wpdb->hide_errors();
 
+if (is_multisite()) {
+    $main_site = get_network()->site_id;
+    $sql = "SELECT blog_id FROM $wpdb->blogs
+                WHERE archived = '0' AND spam = '0'
+                AND deleted = '0'";
 
-    if (is_multisite()) {
-        $main_site = get_network()->site_id;
-        if ( ! function_exists( 'is_plugin_active_for_network' ) ) {
-            require_once( ABSPATH . '/wp-admin/includes/plugin.php' );
-        }
+    $blog_ids = $wpdb->get_col( $sql );
 
-        // Network active.
-        if ( is_plugin_active_for_network( 'userswp/userswp.php' ) ) {
-            if (defined('UWP_ROOT_PAGES')) {
-                if (UWP_ROOT_PAGES == 'all') {
-                    $sql = "SELECT blog_id FROM $wpdb->blogs
-                    WHERE archived = '0' AND spam = '0'
-                    AND deleted = '0'";
-
-                    $blog_ids = $wpdb->get_col( $sql );
-
-                    foreach ( $blog_ids as $blog_id ) {
-                        switch_to_blog( $blog_id );
-                        uwp_uninstall();
-                    }
-                    restore_current_blog();
-                } else {
-                    $blog_id = UWP_ROOT_PAGES;
-                    switch_to_blog( $blog_id );
-                    uwp_uninstall();
-                    restore_current_blog();
-                }
-            } else {
-                switch_to_blog( $main_site );
-                uwp_uninstall();
-                restore_current_blog();
-            }
-
-            switch_to_blog( $main_site );
-            uwp_drop_usermeta_table();
-            restore_current_blog();
-        } else {
-            uwp_uninstall();
-            uwp_drop_usermeta_table();
-        }
-    } else {
+    foreach ( $blog_ids as $blog_id ) {
+        switch_to_blog( $blog_id );
         uwp_uninstall();
-        uwp_drop_usermeta_table();
     }
-
+    switch_to_blog( $main_site );
+    uwp_drop_usermeta_table();
+    restore_current_blog();
+} else {
+    uwp_uninstall();
+    uwp_drop_usermeta_table();
 }
 
 function uwp_uninstall(){
-    global $wpdb;
-    // Delete options
-    delete_option('uwp_settings');
-    delete_option('uwp_activation_redirect');
-    delete_option('uwp_flush_rewrite');
-    delete_option('uwp_default_data_installed');
-    delete_option('uwp_db_version');
+    if ( uwp_get_option('uninstall_erase_data') == '1' ) {
+        global $wpdb;
+
+        // Delete options
+        delete_option('uwp_settings');
+        delete_option('uwp_activation_redirect');
+        delete_option('uwp_flush_rewrite');
+        delete_option('uwp_default_data_installed');
+        delete_option('uwp_db_version');
 
 
-    $table_name = uwp_get_table_prefix() . 'uwp_form_fields';
-    $rows = $wpdb->get_results("select * from " . $table_name . "");
+        $table_name = uwp_get_table_prefix() . 'uwp_form_fields';
+        $rows = $wpdb->get_results("select * from " . $table_name . "");
 
-    // Delete user meta for all users
-    $meta_type  = 'user';
-    $user_id    = 0; // This will be ignored, since we are deleting for all users.
-    $meta_key   = 'uwp_usermeta';
-    $meta_value = ''; // Also ignored. The meta will be deleted regardless of value.
-    $delete_all = true;
+        // Delete user meta for all users
+        $meta_type  = 'user';
+        $user_id    = 0; // This will be ignored, since we are deleting for all users.
+        $meta_key   = 'uwp_usermeta';
+        $meta_value = ''; // Also ignored. The meta will be deleted regardless of value.
+        $delete_all = true;
 
-    foreach ($rows as $row) {
-        delete_metadata( $meta_type, $user_id, $row->htmlvar_name, $meta_value, $delete_all );
+        foreach ($rows as $row) {
+            delete_metadata( $meta_type, $user_id, $row->htmlvar_name, $meta_value, $delete_all );
+        }
+
+        // Drop form fields table
+        $table_name = uwp_get_table_prefix() . 'uwp_form_fields';
+        $sql = "DROP TABLE IF EXISTS $table_name";
+        $wpdb->query($sql);
+
+        // Drop form extras table
+        $extras_table_name = uwp_get_table_prefix() . 'uwp_form_extras';
+        $sql = "DROP TABLE IF EXISTS $extras_table_name";
+        $wpdb->query($sql);
     }
-
-    // Drop tables.
-    // Drop form fields table
-    $table_name = uwp_get_table_prefix() . 'uwp_form_fields';
-    $sql = "DROP TABLE IF EXISTS $table_name";
-    $wpdb->query($sql);
-
-    // Drop form extras table
-    $extras_table_name = uwp_get_table_prefix() . 'uwp_form_extras';
-    $sql = "DROP TABLE IF EXISTS $extras_table_name";
-    $wpdb->query($sql);
 }
 
 function uwp_drop_usermeta_table(){
     global $wpdb;
-
-    // Drop usermeta table
-    $meta_table_name = get_usermeta_table_prefix() . 'uwp_usermeta';
-    $sql = "DROP TABLE IF EXISTS $meta_table_name";
-    $wpdb->query($sql);
+    if ( uwp_get_option('uninstall_erase_data') == '1' ) {
+        // Drop usermeta table
+        $meta_table_name = uwp_get_table_prefix() . 'uwp_usermeta';
+        $sql = "DROP TABLE IF EXISTS $meta_table_name";
+        $wpdb->query($sql);
+    }
 }
