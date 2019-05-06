e132c05be0d7cd694ab14c4dc1f8a1e6cdee00f3
Patrik <squarelinksolutions@gmail.com>
2019-05-06 17:11:08 +0530

    changes for release 1.1.0

--- userswp.php
+++ userswp.php
@@ -3,7 +3,7 @@
 Plugin Name: UsersWP
 Plugin URI: https://userswp.io/
 Description: User management plugin.
-Version: 1.0.24
+Version: 1.1.0
 Author: AyeCode Ltd
 Author URI: https://userswp.io
 License: GPL-2.0+
@@ -24,7 +24,7 @@ if ( ! defined( 'USERSWP_NAME' ) ) {
 }
 
 if ( ! defined( 'USERSWP_VERSION' ) ) {
-    define('USERSWP_VERSION', '1.0.24');
+    define('USERSWP_VERSION', '1.1.0');
 }
 
 if ( ! defined( 'USERSWP_PATH' ) ) {
