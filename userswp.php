bedd85b63e7346e340d71a44a5b2cf19681120c1
Patrik <squarelinksolutions@gmail.com>
2020-12-12 14:46:53 +0530

    Releasing update 1.2.2.11

--- userswp.php
+++ userswp.php
@@ -3,7 +3,7 @@
 Plugin Name: UsersWP
 Plugin URI: https://userswp.io/
 Description: The only lightweight user profile plugin for WordPress. UsersWP features front end user profile, users directory, a registration and a login form.
-Version: 1.2.2.10
+Version: 1.2.2.11
 Author: AyeCode Ltd
 Author URI: https://userswp.io
 License: GPL-2.0+
@@ -24,7 +24,7 @@ if ( ! defined( 'USERSWP_NAME' ) ) {
 }
 
 if ( ! defined( 'USERSWP_VERSION' ) ) {
-	define( 'USERSWP_VERSION', '1.2.2.10' );
+	define( 'USERSWP_VERSION', '1.2.2.11' );
 }
 
 if ( ! defined( 'USERSWP_PATH' ) ) {
