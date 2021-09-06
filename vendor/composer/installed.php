8c5ca17133ad36dca8e1f3a5d67464d888b1b029
Patrik <squarelinksolutions@gmail.com>
2021-09-06 16:46:23 +0530

    Releasing update 1.2.2.30

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,89 +1,92 @@
-<?php return array(
-    'root' => array(
-        'pretty_version' => 'dev-master',
-        'version' => 'dev-master',
-        'type' => 'project',
-        'install_path' => __DIR__ . '/../../',
-        'aliases' => array(),
-        'reference' => '3d01df9b7e311d82afdb6e9704b8449ba17818e1',
-        'name' => 'uswerwp/userswp',
-        'dev' => true,
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
     ),
-    'versions' => array(
-        'ayecode/ayecode-connect-helper' => array(
-            'pretty_version' => '1.0.3',
-            'version' => '1.0.3.0',
-            'type' => 'library',
-            'install_path' => __DIR__ . '/../ayecode/ayecode-connect-helper',
-            'aliases' => array(),
-            'reference' => '1af7cdefdbd20d4443a3ab4834e4c1cd8fe57fb4',
-            'dev_requirement' => false,
-        ),
-        'ayecode/wp-ayecode-ui' => array(
-            'pretty_version' => '0.1.61',
-            'version' => '0.1.61.0',
-            'type' => 'library',
-            'install_path' => __DIR__ . '/../ayecode/wp-ayecode-ui',
-            'aliases' => array(),
-            'reference' => 'e97d1e520d6722df2cb323c04a6e818b1e901ab5',
-            'dev_requirement' => false,
-        ),
-        'ayecode/wp-deactivation-survey' => array(
-            'pretty_version' => '1.0.3',
-            'version' => '1.0.3.0',
-            'type' => 'library',
-            'install_path' => __DIR__ . '/../ayecode/wp-deactivation-survey',
-            'aliases' => array(),
-            'reference' => 'c4b0ba914835f17dca0cf69fe621c2db491d4667',
-            'dev_requirement' => false,
-        ),
-        'ayecode/wp-font-awesome-settings' => array(
-            'pretty_version' => '1.0.13',
-            'version' => '1.0.13.0',
-            'type' => 'library',
-            'install_path' => __DIR__ . '/../ayecode/wp-font-awesome-settings',
-            'aliases' => array(),
-            'reference' => 'a7a11ee4290674ec214d1fe694139af275350402',
-            'dev_requirement' => false,
-        ),
-        'ayecode/wp-super-duper' => array(
-            'pretty_version' => '1.0.27',
-            'version' => '1.0.27.0',
-            'type' => 'library',
-            'install_path' => __DIR__ . '/../ayecode/wp-super-duper',
-            'aliases' => array(),
-            'reference' => 'ce187bc9afc5cd7f5d790d84eb6fd9b98e56992d',
-            'dev_requirement' => false,
-        ),
-        'composer/installers' => array(
-            'pretty_version' => '1.x-dev',
-            'version' => '1.9999999.9999999.9999999-dev',
-            'type' => 'composer-plugin',
-            'install_path' => __DIR__ . '/./installers',
-            'aliases' => array(),
-            'reference' => '1e8a1614adc33d455f81e9b5381d4f661565fe90',
-            'dev_requirement' => false,
-        ),
-        'roundcube/plugin-installer' => array(
-            'dev_requirement' => false,
-            'replaced' => array(
-                0 => '*',
-            ),
-        ),
-        'shama/baton' => array(
-            'dev_requirement' => false,
-            'replaced' => array(
-                0 => '*',
-            ),
-        ),
-        'uswerwp/userswp' => array(
-            'pretty_version' => 'dev-master',
-            'version' => 'dev-master',
-            'type' => 'project',
-            'install_path' => __DIR__ . '/../../',
-            'aliases' => array(),
-            'reference' => '3d01df9b7e311d82afdb6e9704b8449ba17818e1',
-            'dev_requirement' => false,
-        ),
+    'reference' => 'c7d4f8eda2e4932d0f6515c114d9d5e75c1d5b6f',
+    'name' => 'uswerwp/userswp',
+  ),
+  'versions' => 
+  array (
+    'ayecode/ayecode-connect-helper' => 
+    array (
+      'pretty_version' => '1.0.3',
+      'version' => '1.0.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '1af7cdefdbd20d4443a3ab4834e4c1cd8fe57fb4',
     ),
+    'ayecode/wp-ayecode-ui' => 
+    array (
+      'pretty_version' => '0.1.61',
+      'version' => '0.1.61.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'e97d1e520d6722df2cb323c04a6e818b1e901ab5',
+    ),
+    'ayecode/wp-deactivation-survey' => 
+    array (
+      'pretty_version' => '1.0.3',
+      'version' => '1.0.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c4b0ba914835f17dca0cf69fe621c2db491d4667',
+    ),
+    'ayecode/wp-font-awesome-settings' => 
+    array (
+      'pretty_version' => '1.0.13',
+      'version' => '1.0.13.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'a7a11ee4290674ec214d1fe694139af275350402',
+    ),
+    'ayecode/wp-super-duper' => 
+    array (
+      'pretty_version' => '1.0.27',
+      'version' => '1.0.27.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'ce187bc9afc5cd7f5d790d84eb6fd9b98e56992d',
+    ),
+    'composer/installers' => 
+    array (
+      'pretty_version' => '1.x-dev',
+      'version' => '1.9999999.9999999.9999999-dev',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '1e8a1614adc33d455f81e9b5381d4f661565fe90',
+    ),
+    'roundcube/plugin-installer' => 
+    array (
+      'replaced' => 
+      array (
+        0 => '*',
+      ),
+    ),
+    'shama/baton' => 
+    array (
+      'replaced' => 
+      array (
+        0 => '*',
+      ),
+    ),
+    'uswerwp/userswp' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c7d4f8eda2e4932d0f6515c114d9d5e75c1d5b6f',
+    ),
+  ),
 );
