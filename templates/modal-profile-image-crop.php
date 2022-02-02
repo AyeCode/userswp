6f41ebd2791ae2ea94a51c7e5b255a24509909e8
Patrik <squarelinksolutions@gmail.com>
2022-02-02 20:21:39 +0530

    Changes while testing.

--- templates/modal-profile-image-crop.php
+++ templates/modal-profile-image-crop.php
@@ -36,7 +36,7 @@ $image_url = !empty($args['image_url']) ? esc_url( $args['image_url'] ) : '';
 							<input type="hidden" name="w" value="" id="<?php echo $type; ?>-w" />
 							<input type="hidden" name="h" value="" id="<?php echo $type; ?>-h" />
 							<input type="hidden" id="uwp-<?php echo $type; ?>-crop-image" name="uwp_crop" value="<?php echo $image_url; ?>" />
-							<input type="hidden" name="uwp_crop_nonce" value="<?php echo wp_create_nonce( 'uwp_crop_nonce_'.$type.'_'.get_current_user_id() ); ?>" />
+							<input type="hidden" name="uwp_crop_nonce" value="<?php echo wp_create_nonce( 'uwp_crop_nonce_'.$type ); ?>" />
 							<input type="submit" name="uwp_<?php echo $type; ?>_crop" value="<?php _e('Apply', 'userswp'); ?>" class="button button-primary" id="save_uwp_<?php echo $type; ?>" />
 						</form>
 					</div>
