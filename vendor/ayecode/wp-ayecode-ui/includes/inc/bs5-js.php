b7da737e61cb33136ea5b1a67f281b20b6f4768f
Kiran Prajapati <officeofkiran@gmail.com>
2024-11-12 20:14:50 +0530

    Merge AUI 0.2.28 & SD 1.2.14

--- vendor/ayecode/wp-ayecode-ui/includes/inc/bs5-js.php
+++ vendor/ayecode/wp-ayecode-ui/includes/inc/bs5-js.php
@@ -637,7 +637,8 @@
 
             // items
             $i = 0;
-            $carousel += '<div class="carousel-inner d-flex align-items-center">';
+            $rtl_class = '<?php echo is_rtl() ? 'justify-content-end' : 'justify-content-start'; ?>'; 
+            $carousel += '<div class="carousel-inner d-flex align-items-center ' + $rtl_class + '">';
             $container.find('.aui-lightbox-image').each(function() {
                 var a = this;
                 var href = jQuery(a).attr('href');
--- vendor/ayecode/wp-super-duper/sd-plugin.php
+++ vendor/ayecode/wp-super-duper/sd-plugin.php
@@ -5,7 +5,7 @@
  * @wordpress-plugin
  * Plugin Name: Super Duper - Examples
  * Description: This is a Hello World test plugin for WP Super Duper Class.
- * Version: 1.2.13
+ * Version: 1.2.14
  * Author: AyeCode
  * Author URI: https://ayecode.io
  * Text Domain: super-duper
